54476d0ab9546dca8e87462d76c4e0557591ce63
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-04-18 16:30:40 -0500

    support new api

--- notebookworkshoploader/loader.py
+++ notebookworkshoploader/loader.py
@@ -7,24 +7,34 @@ import ipywidgets as widgets
 from ipywidgets import HBox, VBox
 from IPython.display import display
 
+NEW_APISERVER_URL = "https://workshop-setup-api-voldmqr2bq-uc.a.run.app"
+
 def load_remote_env(*, env_url, file=None):
 
-    def load(key):
+    def validate_workshop(key, api_server_url):
         try:
             if file is not None:
-                resp = requests.get(f'{env_url}/api/workshop?key={key}&file={os.path.basename(file)}', timeout=5)
+                resp = requests.get(f'{api_server_url}/api/workshop?key={key}&file={os.path.basename(file)}', timeout=5)
             else:
-                resp = requests.get(f'{env_url}/api/workshop?key={key}', timeout=5)
-            if resp.status_code == 200:
-                env_encoded = resp.json()['env_encoded']
-                load_dotenv(stream=StringIO(base64.b64decode(env_encoded).decode()), override=True)
-                print(f"Successfully loaded environment variables for the {os.environ['WORKSHOP_NAME']} workshop from remote env file")
-            elif resp.status_code == 403:
-                print('workshop expired')
-            elif resp.status_code == 404:
-                print('unknown workshop')
+                resp = requests.get(f'{api_server_url}/api/workshop?key={key}', timeout=5)
+            return resp
         except Exception as e:
-            print('Unknown error trying to load workshop environment variables', e)
+            return e
+
+    def load(key):
+        resp = validate_workshop(key, NEW_APISERVER_URL)
+        if env_url != NEW_APISERVER_URL and (isinstance(resp, Exception) or resp.status_code == 404):
+            resp = validate_workshop(key, env_url)
+        if isinstance(resp, Exception):
+            print('Unknown error trying to load workshop environment variables', resp)
+        elif resp.status_code == 200:
+            env_encoded = resp.json()['env_encoded']
+            load_dotenv(stream=StringIO(base64.b64decode(env_encoded).decode()), override=True)
+            print(f"Successfully loaded environment variables for the {os.environ['WORKSHOP_NAME']} workshop from remote env file")
+        elif resp.status_code == 403:
+            print('workshop expired')
+        elif resp.status_code == 404:
+            print('unknown workshop')
 
     def on_load(b):
         load(workshop_key.value)
@@ -40,4 +50,3 @@ def load_remote_env(*, env_url, file=None):
         hbox = HBox([workshop_key, load_workshop])
         vbox = VBox([instructions, hbox])
         display(vbox)
-    
